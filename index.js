3059783fc61f1c6acce8429e97739c6f264943c6
Blake Stephens <blake.stephens@lge.com>
2016-05-11 13:11:23 -0700

    Initial commit. These rules will likely change soon.
    Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = {
+	globals: {
+		"require": true
+	},
+	"parserOptions": {
+		"sourceType": "module"
+	},
+	rules: {
+		"comma-dangle": [2,"always"],
+		"no-unused-vars": 2,
+		"no-use-before-define": 2,
+		"semi": 2,
+		"eqeqeq": "off",
+		"curly": "error"
+	}
+};
