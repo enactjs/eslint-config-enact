179627dcf04f6fe8b26c3aa4e7a5ab20492f1615
Blake Stephens <blake.stephens@lge.com>
2016-05-13 11:44:41 -0700

    Added support for eslint-plugin-react
    Enyo-DCO-1.1-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 module.exports = {
 	globals: {
-		'require': true
+		'require': true,
+		'module': true
 	},
+	extends: ['eslint:recommended', 'plugin:react/recommended'],
 	parserOptions: {
 		'ecmaVersion': 6,
 		'sourceType': 'module',
 		'ecmaFeatures': {
 			'jsx': true
+		}
+	},
+	plugins: [
+		'react'
+	],
+	settings: {
+		'react': {
+			'pragma': 'React',  // Pragma to use, default to "React"
+			'version': '15.0' // React version, default to the latest React stable release
 		}
 	},
 	rules: {
