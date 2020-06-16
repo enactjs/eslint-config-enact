6979305f90341a729e69e05b295161281ce70c05
Jason Robitaille <robitaille88@gmail.com>
2020-06-16 09:07:38 -0700

    jsx-a11y support

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ module.exports = {
 			jsx: true
 		}
 	},
-	plugins: ['babel', 'react', 'enact'],
+	plugins: ['babel', 'react', 'jsx-a11y', 'enact'],
 	settings: {
 		react: {
 			'pragma': 'React',	// Pragma to use, default to "React"
@@ -147,7 +147,30 @@ module.exports = {
 		'babel/no-unused-expressions': 'warn',
 
 		// enact plugin https://github.com/enactjs/eslint-plugin-enact/
-		'enact/no-module-exports-import': 'error'
+		'enact/no-module-exports-import': 'error',
+
+
+		// https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules
+		'jsx-a11y/accessible-emoji': 'warn',
+		'jsx-a11y/alt-text': 'warn',
+		'jsx-a11y/anchor-has-content': 'warn',
+		'jsx-a11y/anchor-is-valid': ['warn', {
+			aspects: ['noHref', 'invalidHref']
+		}],
+		'jsx-a11y/aria-activedescendant-has-tabindex': 'warn',
+		'jsx-a11y/aria-props': 'warn',
+		'jsx-a11y/aria-proptypes': 'warn',
+		'jsx-a11y/aria-role': ['warn', {ignoreNonDOM: true}],
+		'jsx-a11y/aria-unsupported-elements': 'warn',
+		'jsx-a11y/heading-has-content': 'warn',
+		'jsx-a11y/iframe-has-title': 'warn',
+		'jsx-a11y/img-redundant-alt': 'warn',
+		'jsx-a11y/no-access-key': 'warn',
+		'jsx-a11y/no-distracting-elements': 'warn',
+		'jsx-a11y/no-redundant-roles': 'warn',
+		'jsx-a11y/role-has-required-aria-props': 'warn',
+		'jsx-a11y/role-supports-aria-props': 'warn',
+		'jsx-a11y/scope': 'warn'
 	},
 	overrides: [
 		{
