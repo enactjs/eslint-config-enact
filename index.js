ad7f573d0e25c27ba77d143f48624475df467415
Jason Robitaille <robitaille88@gmail.com>
2020-06-26 09:52:05 -0700

    Add support for jsx-a11y rules and add react/forbid-foreign-prop-types warning as a sefety for Babel production proptype removal

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ module.exports = {
 			jsx: true
 		}
 	},
-	plugins: ['babel', 'react', 'enact'],
+	plugins: ['babel', 'jsx-a11y',  'react', 'enact'],
 	settings: {
 		react: {
 			'pragma': 'React',	// Pragma to use, default to "React"
@@ -107,6 +107,28 @@ module.exports = {
 		'use-isnan': 'warn',
 		'wrap-iife': ['error', 'inside'],
 
+		// jsx-a11y plugin https://github.com/evcohen/eslint-plugin-jsx-a11y
+		'jsx-a11y/accessible-emoji': 'warn',
+		'jsx-a11y/alt-text': 'warn',
+		'jsx-a11y/anchor-has-content': 'warn',
+		'jsx-a11y/anchor-is-valid': ['warn', {
+			aspects: ['noHref', 'invalidHref']
+		}],
+		'jsx-a11y/aria-activedescendant-has-tabindex': 'warn',
+		'jsx-a11y/aria-props': 'warn',
+		'jsx-a11y/aria-proptypes': 'warn',
+		'jsx-a11y/aria-role': ['warn', {ignoreNonDOM: true}],
+		'jsx-a11y/aria-unsupported-elements': 'warn',
+		'jsx-a11y/heading-has-content': 'warn',
+		'jsx-a11y/iframe-has-title': 'warn',
+		'jsx-a11y/img-redundant-alt': 'warn',
+		'jsx-a11y/no-access-key': 'warn',
+		'jsx-a11y/no-distracting-elements': 'warn',
+		'jsx-a11y/no-redundant-roles': 'warn',
+		'jsx-a11y/role-has-required-aria-props': 'warn',
+		'jsx-a11y/role-supports-aria-props': 'warn',
+		'jsx-a11y/scope': 'warn'
+
 		// react plugin https://github.com/yannickcr/eslint-plugin-react
 		'react/display-name': 'off',
 		'react/no-access-state-in-setstate': 'warn',
@@ -117,6 +139,7 @@ module.exports = {
 		'react/no-did-update-set-state': ['warn'],
 		'react/no-direct-mutation-state': 'warn',
 		'react/no-find-dom-node': 'warn',
+		'react/forbid-foreign-prop-types': 'warn',
 		'react/no-is-mounted': 'warn',
 		'react/no-render-return-value': 'warn',
 		'react/no-string-refs': 'warn',
