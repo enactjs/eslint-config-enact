0a7d13eb4f9ab6ac99742323965610e1b9a3177a
taeyoung.hong <35059065+hong6316@users.noreply.github.com>
2023-06-16 13:29:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// This is a workaround for https://github.com/eslint/eslint/issues/3458
+// Fix eslint shareable config (https://github.com/eslint/eslint/issues/3458)
 require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
